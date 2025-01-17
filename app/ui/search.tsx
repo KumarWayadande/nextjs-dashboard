d3480e732e4e1d6a642d07ba31cfc949e0263842
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-17 15:45:11 +0530

    modified:   app/ui/search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useDebouncedCallback } from "use-debounce";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Query } from "@vercel/postgres";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
@@ -9,8 +9,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
 
     if (term) {
       params.set("query", term);
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
